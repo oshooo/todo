56634b89d4a2d13f1a6b1fc9b8d6be352cbf522e
Ridwan Osho <oshoridwan@gmail.com>
2021-04-08 11:44:28 +0100

    Changed Outline

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
     const list = document.querySelector('.list');
     const date = document.querySelector('.date');
     const saveTask = document.querySelector('.save');
+    const form = document.querySelector('form');
+    const btn = document.querySelector('button');
+    const txt_input = document.querySelector('input');
 
 //Date
     const d = new Date();
@@ -17,38 +20,15 @@
     
     
 //Event Listeners
-
+    btn.addEventListener('click', saveTasks)
     addTask.addEventListener('click', addNewTask);
-    saveTask.addEventListener('click', saveTasks);
     document.body.addEventListener('click', taskDone);
     document.body.addEventListener('click', deleteTask);
 
     
 //Functions
     function addNewTask(){
-        const newTask = document.createElement('li');
-        newTask.className = 'task';
-        newTask.contentEditable = true;
-        newTask.focus = true;
-        let taskVal = newTask.value;
-        
-        const del = document.createElement('span');
-        del.className = 'delete';
-        del.innerHTML = '✘';
-        const done = document.createElement('span');
-        done.className = 'done';
-        done.innerHTML = '✔';
-        const actions = document.createElement('div');
-        actions.className = 'action';
-
-        const item = document.createElement('div');
-
-        item.className = 'item';
-        item.appendChild(newTask);
-        actions.appendChild(del);
-        actions.appendChild(done);
-        item.appendChild(actions);
-        list.appendChild(item);
+        form.style.visibility = 'visible';
     }
 
     function taskDone(e){
@@ -65,6 +45,50 @@
         
     } 
 
-    function saveTasks(){
-        alert("Hello");
+    function saveTasks(e){
+        e.preventDefault();
+        const newTask = document.createElement('li');
+        newTask.className = 'task';
+        const taskVal = txt_input.value;
+        newTask.innerText = taskVal;
+        form.style.visibility = 'hidden';
+
+        //delete Task
+        const del = document.createElement('span');
+        del.className = 'delete';
+        del.innerHTML = '✘';
+        
+        //mark task as done
+        const done = document.createElement('span');
+        done.className = 'done';
+        done.innerHTML = '✔';
+        const actions = document.createElement('div');
+        actions.className = 'action';
+
+        const item = document.createElement('div');
+
+        item.className = 'item';
+        item.appendChild(newTask);
+        actions.appendChild(del);
+        actions.appendChild(done);
+        item.appendChild(actions);
+        list.appendChild(item);
+       
+        //saves tweet to local storage
+        addTaskToLocalStorage(taskVal)
+
+        txt_input.value = "";
+    }
+
+    function addTaskToLocalStorage (taskVal) {
+        
+    }
+
+    function getTasksFromStorage () {
+        let tasks;
+
+        if(localStorage.getItem('tasks') == null) {
+            
+        }
+         
     }
